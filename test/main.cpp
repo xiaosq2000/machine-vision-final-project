a3fb0a18a382ade61b561cf43f2490b65ffdb444
Shuqi Xiao <xiaosq2000@gmail.com>
2021-06-07 14:24:12 +0800

    application

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+
+/**
+ * @file main.cpp
+ * @author 
+ * @brief "1D测量类"的测试
+ * @version 0.1
+ * @date 2021-06-07
+ * 
+ */
+#include "measurement.hpp"
+
+int main()
+{
+    Measurement test;
+    test.Line();
+    test.Semicircle();
+    return 0;
+}
